955f48747dd5153fd0d14d6fb871fa8cd4b25da0
VectorHector <erik.doering@t-online.de>
2025-10-13 16:02:52 +0200

    add icons to home tabs

--- src/features/home/layouts/HomeLayout.tsx
+++ src/features/home/layouts/HomeLayout.tsx
@@ -3,6 +3,7 @@
 import { Box } from '@mui/material';
 import { FC, ReactNode } from 'react';
 import { usePathname } from 'next/navigation';
+import { Home, Settings, Event } from '@mui/icons-material';
 
 import { useMessages } from 'core/i18n';
 import messageIds from '../l10n/messageIds';
@@ -34,16 +35,19 @@ const HomeLayout: FC<Props> = ({ children, title }) => {
             href: `/my/home`,
             label: messages.tabs.home(),
             value: 'home',
+            icon: Home,
           },
           {
             href: `/my/feed`,
             label: messages.tabs.feed(),
             value: 'feed',
+            icon: Event,
           },
           {
             href: `/my/settings`,
             label: messages.tabs.settings(),
             value: 'settings',
+            icon: Settings,
           },
         ]}
         title={title || messages.title()}
