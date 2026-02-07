028449dfc9dffb37f76c4d1e7e86c84faf9a5a49
Vector-Hector <30576258+Vector-Hector@users.noreply.github.com>
2026-02-07 03:57:39 +0100

    add dark mode to more zui components

--- src/zui/ZUIAutoTextArea/index.tsx
+++ src/zui/ZUIAutoTextArea/index.tsx
@@ -25,17 +25,20 @@ const ZUIAutoTextArea = React.forwardRef<
         onChange(e.target.value)
       }
       placeholder={placeholder}
-      sx={{
+      sx={(theme) => ({
+        backgroundColor:
+          theme.palette.mode === 'dark' ? theme.palette.grey[900] : 'white',
         border: '2px dotted',
         borderColor: lighten(oldTheme.palette.primary.main, 0.65),
         borderRadius: '10px',
+        color: theme.palette.text.primary,
         fontFamily: oldTheme.typography.fontFamily,
         lineHeight: '1.5',
         overflow: 'hidden',
         padding: '10px',
         resize: 'none',
         width: '100%',
-      }}
+      })}
       value={value}
       {...restProps}
     />
